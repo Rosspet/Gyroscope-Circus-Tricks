a8541bf9dd1d800f61e58b642aaa0410c3588ec6
Ross Petridis <71374438+Lazazz@users.noreply.github.com>
2022-04-20 18:23:15 +1000

    added code from workshop and example

---
+++ animation.m
@@ -0,0 +1,41 @@
+clc
+clear all 
+close all
+
+%% Measuremeants (in mm)
+h_rod = 87;  % 93 - 2*3
+H_rot = 59; % 65  - 2*R_min_V_tor
+R_sph = 2; % 4 accros but 3 high. (R is half)
+R_maj_V_tor = 59/2 + 1.5 ; % h_rot / 2 + R_min_V_tor
+
+R_maj_H_tor = 64; % = 68 - 2*R_min_H_tor
+r_rot = 2 ;
+
+R_min_H_tor = 1; % maybe 2 for radius
+R_min_V_tor = 1.5; %  maybe 3
+R_maj_rotor = 27; %maybe 28
+% r_rot specified in other view above
+R_min_rotor = 3.5;
+
+
+m_rotor = 45; %grams
+m_frame = 23; %grams
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+%%
