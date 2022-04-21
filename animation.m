3fdd1ff2506c978b98cc5138fea6a090d763771f
Ross Petridis <71374438+Lazazz@users.noreply.github.com>
2022-04-21 13:17:33 +1000

    done checking, time to put in correct form for solving

--- animation.m
+++ animation.m
@@ -59,37 +59,70 @@ w4_4 = w43_4 + w3_4;
 
 %% NE Equations
 
-% FRAME SUMF
+%% ROTOR FORCES.
+
+% FORCES
+L = (h_rod-H_rot)/2 + H_rot/2; % Looking at canvas diagram.
+rOG_4 = [0 0 L]; % z3,4 are aligneedd.
+rOG_dot_4 = diff(rOG_4,t) + cross(w4_4, rOG_4);
+rOG_dot_dot_4 = diff(rOG_dot_4,t) + cross(w4_4, rOG_dot_4);
+p_rotor_dot_4 = m_rotor*rOG_dot_dot_4; % shares G with frame.
+Fg_rotor_0 = [0;0;-g*m_rotor];
+Fg_rotor_3 = R30*Fg_rotor_0;
+
+p_rotor_dot_3 = R34*p_rotor_dot_4; %  into {3} as usin {3} for gyroscope frame
+F2_3 = [Fx3; Fy3; Fz3]; %force of frame on rotor in {3}
+% SOLVE BELOW
+p_rotor_dot_3 == Fg_rotor_3 + F2_3; % solve this
+
+%% ROTOR MOMENTS 
+rotor_h = R_min_rotor*2; % height of rotor as we are modelling as cylinder not torus.
+Irotor_G_4 = [
+    (1/12)*m_rotor*(3*R_maj_rotor^2+rotor_h^2) 0 0;
+    0 (1/12)*m_rotor*(3*R_maj_rotor^2+rotor_h^2) 0;
+    0 0 (1/2)*m_rotor*R_maj_rotor^2;
+    ];
+hrotor_G_4 = Irotor_G_4*w4_4;
+hrotor_G_dot_4 = diff(hrotor_G_4,t) + cross(w4_4, hrotor_G_4);
+hrotor_G_dot_3 = R34*hrotor_G_dot_4;
+
+% 3 more eqns
+% SOLVE BELOW
+M_frame_on_rotor_3 = [Mx3; My3; 0];
+
+% 3 more eqns
+% SOLVE BELOW
+hrotor_G_dot_3 == M_frame_on_rotor_3;
+
+
+%% FRAME FORCES
 %volume_frame_horizontal_ring = 2*pi^2*r*2*R_o; 
 %volume_frame_vertical_ring = 2*pi^2*r*2*R_o;
 %volume_frame = volume_frame_vertical_ring + volume_frame_horizontal_ring;
-m_frame = 23; % GRAMS % (physically weighed this - dont need formula. rho*volume_frame;
+
 rOG_3 = [0 0 L];
 rOG_dot_3 = diff(rOG_3,t) + cross(w3_3, rOG_3);
-rOG_dot_dot_3 = diff(rOG__dot_3,t) + cross(w3_3, rOG__dot_3);
+rOG_dot_dot_3 = diff(rOG_dot_3,t) + cross(w3_3, rOG_dot_3);
 p_frame_dot_3 = m_frame*rOG_dot_dot_3; % RHS
 
-%LHS - do everything in {3} as both forces are in this and can convert
-%gravity easily.
+%LHS - do everything in {3} as p and most forces in 3 except Fg which is
+%only 1 component to convert.
 F1_3 = [Fx1_3; Fy1_3; Fz1_3]; % stand on frame 
-F2_3 = [Fx2_3; Fy2_3; Fz2_3]; % rotor on frame @ G.
-
+%recall F2_3 is frame on rotor, so negate to get rotor on frame.
 Fg_frame_0 = [0;0;-g*m_frame]; % gravity on frame.
-Fg_frame_3 = R30*Fg_frame_0;
+Fg_frame_3 = R30*Fg_frame_0; % gravity of frame in {3}
 
 %summation (3 Equations)
-F1_3 + Fg_frame_3 + F2_3 = p_frame_dot_3; % solve this
+F1_3 + Fg_frame_3 - F2_3 == p_frame_dot_3; % solve this
 
-% FRAME MOMENTS
+%% FRAME MOMENTS
 % going to do about G which is assume COM due to it being in the centre
 % of both rings and the pole. and wont need to use parallel axis thm.
 
-rGO_3 = -rOG_3;
 
-% hframe_G_3 angular momentum of frame about G in {3}.
 
 %perfect cylinder of height H (h_rod), radius, r_rot
-%(currently omitting the 2 small spheres for simplicity but can add later 
+%(currently omitting the 2 small spheres in Height for simplicity but can add later 
 % if deemed significant, i.e., non negligible.)
 vol_pole = h_rod*pi*r_rot^2;
 m_pole = rho*vol_pole;
@@ -100,7 +133,7 @@ Ipole_G_3 = [(1/12)*m_pole*(3*r_rot^2+h_rod^2) 0 0;
         
 % the ring which goes horizontally - modeled as a torus
 % rho % density of rings and pole.
-vol_ring_H= (pi*R_min_H_tor^2)*(2*pi*R_maj_H_tor);
+vol_ring_H= (pi*R_min_H_tor^2)*(2*pi*R_maj_H_tor); %google
 m_ring_H = rho*vol_ring_H;    
 IhoriRing_G_3 = [
     (1/8)*m_ring_H*(4*R_maj_H_tor^2 + 5*R_min_H_tor^2) 0 0;
@@ -120,45 +153,14 @@ IvertRing_G_3 = [
 Iframe_G_3 = IvertRing_G_3 + IhoriRing_G_3 + Ipole_G_3;
 
 hframe_G_3 = Iframe_G_3*w3_3;
-
 hframe_G_3_dot = diff(hframe_G_3,t) + cross(w3_3, hframe_G_3);
-
-M2_3 = [Mx3; My3; 0]; % moment of rotor on on frame
+rGO_3 = -rOG_3;
 
 % sumM of Frame about G - 3 eqns - all 3 EOM once put in values for F1_3
-cross(rGO_3, F1_3) + M2_3 = hframe_G_3_dot; % solve this
+cross(rGO_3, F1_3) - M_frame_on_rotor_3 == hframe_G_3_dot; % solve this
 
 
 
-%% ROTOR NE.
-
-% FORCES
-rOG_4 = [0 0 L]; % z3,4 are aligneedd.
-rOG_dot_4 = diff(rOG_4,t) + cross(w4_4, rOG_4);
-rOG_dot_dot_4 = diff(rOG_dot_4,t) + cross(w4_4, rOG_dot_4);
-p_rotor_dot_4 = m_rotor*rOG_dot_dot_4; % same G as frame.
-Fg_rotor_0 = [0;0;-g*m_rotor];
-Fg_rotor_3 = R30*Fg_rotor_0;
-
-p_rotor_dot_3 = R34*p_rotor_dot_4
-
-% 3 more eqns
-% does it need to be done in {4} then rotated out? YES
-p_rotor_dot_3 == Fg_rotor_3 - F2_3; % solve this
-
-% MOMENTS
-rotor_h = R_min_rotor*2; % height of rotor as we are modelling as cylinder not torus.
-Irotor_G_4 = [
-    (1/12)*m_rotor*(3*R_maj_rotor^2+rotor_h^2) 0 0;
-    0 (1/12)*m_rotor*(3*R_maj_rotor^2+rotor_h^2) 0;
-    0 0 (1/2)*m_rotor*R_maj_rotor^2;
-    ];
-hrotor_G_4 = Irotor_G_4*w4_4;
-hrotor_G_4_dot = diff(hrotor_G_4,t) + cross(w4_4, hrotor_G_4);
-hrotor_G_3_dot = R34*hrotor_G_4_dot;
-
-% 3 more eqns
-hrotor_G_3_dot == -M2_3;
 
 
 
