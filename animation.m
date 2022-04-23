90849da50c4f2757547e642a3c7dfc68ea6da4af
Ross Petridis <71374438+Lazazz@users.noreply.github.com>
2022-04-23 12:47:09 +1000

    cleaning up for max

--- animation.m
+++ animation.m
@@ -2,8 +2,7 @@ clc
 
 
 %%  3. State vector
-
-
-X = [alpha_; beta_; gamma_; delta_; diff(alpha_,t); diff(beta_,t); diff(gamma_,t); diff(delta_,t)];
+% (see getXdot file for this)
+%X = [alpha_; beta_; gamma_; delta_; diff(alpha_,t); diff(beta_,t); diff(gamma_,t); diff(delta_,t)];
 
 
