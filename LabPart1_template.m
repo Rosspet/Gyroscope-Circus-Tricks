87c44fd4f7dee3e837e2a1bc77d0aae723c02d9e
mwajones <54984062+mwajones@users.noreply.github.com>
2022-05-09 13:15:30 +1000

    Final

--- LabPart1_template.m
+++ LabPart1_template.m
@@ -1,5 +1,3 @@
-clear all; clc;
-
 syms t real
 
 %alpha__, beta_, gamma_ and delta_ time functions
@@ -82,18 +80,12 @@ IGtorV_3 = [
 
 % Inertia tensor of the rod about its center of mass expressed in frame 3 
 % Variable Name: IGrod_3
-IGrod_rot = [(1/12)*m_rod_rotor*(3*r_rot^2+H_rot^2) 0 0; 
-            0 (1/12)*m_rod_rotor*(3*r_rot^2+H_rot^2) 0 
-            0 0 (1/2)*m_rod_rotor*r_rot^2 
+m_rod = m_rod_rotor+m_rod_frame;
+IGrod_3 = [
+            (1/12)*m_rod*(3*r_rot^2+h_rod^2) 0 0; 
+            0 (1/12)*m_rod*(3*r_rot^2+h_rod^2) 0 
+            0 0 (1/2)*m_rod*r_rot^2 
             ];
-
-IGrod_frame = [(1/12)*(m_rod_frame/2)*(3*r_rot^2+((h_rod-H_rot)/2)^2) 0 0; 
-            0 (1/12)*(m_rod_frame/2)*(3*r_rot^2+((h_rod-H_rot)/2)^2) 0 
-            0 0 (1/2)*(m_rod_frame/2)*r_rot^2 
-            ];
-
-delta_z = (L-(h_rod-H_rot)/4);
-IGrod_3 = 2*IGrod_frame+m_rod_frame*[delta_z^2, 0, 0; 0, delta_z^2, 0; 0, 0, 0]+IGrod_rot;
 
 % Inertia tensor of the frame about its center of mass expressed in frame 3 (Test)
 % Variable Name: IGframe_3
@@ -247,111 +239,9 @@ ang_NE_frame = Mframe_3 == hGframe_3_dot+Mrotor_3-cross(rGO_3, Fframe_3);
 
 equations = zeros(4,1);
 
-% this doesnt work. for example, the last moments, M_Ox, has other
-% symbolics in their expression which should be expanded.
-% [F_Gx F_Gy F_Gz] = solve(lin_NE_rotor, [F_Gx F_Gy F_Gz]); % Frotor_4 = [F_Gx; F_Gy; F_Gz];
-% [F_Ox F_Oy F_Oz] = solve(lin_NE_frame, [F_Ox F_Oy F_Oz]); % Fframe_3 = [F_Ox; F_Oy; F_Oz];
-% [M_Gx M_Gy M_Gz] = solve(ang_NE_rotor, [M_Gx M_Gy M_Gz]); % Mrotor_4 = [M_Gx; M_Gy; M_Gz];
-% [M_Ox M_Oy M_Oz] = solve(ang_NE_frame, [M_Ox M_Oy M_Oz]); %Mframe_3 = [M_Ox; M_Oy; M_Oz];
-
-[F_Gx, F_Gy, F_Gz] = solve(Frotor_4 == m_rotor*rOG_4_dotdot-FGrotor_4, [F_Gx, F_Gy, F_Gz]); % Frotor_4 = [F_Gx; F_Gy; F_Gz];
-frotor_4 = [simplify(F_Gx); simplify(F_Gy); simplify(F_Gz)]; % we solve for this but isnt used in the equation below as we take angular moment about point this force act on, on the frame.
-frotor_3 = R34*frotor_4;
-
-[F_Ox, F_Oy, F_Oz] = solve(Fframe_3 == m_frame*rOG_3_dotdot-FGframe_3+frotor_3, [F_Ox, F_Oy, F_Oz]); % Fframe_3 = [F_Ox; F_Oy; F_Oz];
-fframe_3 = [simplify(F_Ox); simplify(F_Oy); simplify(F_Oz)];
-
-[M_Gx, M_Gy, M_Gz] = solve(Mrotor_4 == hGrotor_4_dot, [M_Gx, M_Gy, M_Gz]); % Mrotor_4 = [M_Gx; M_Gy; M_Gz];
-mrotor_4 = [simplify(M_Gx); simplify(M_Gy); simplify(M_Gz)];
-mrotor_3=R34*mrotor_4;
-
-[M_Ox, M_Oy, M_Oz] = solve(Mframe_3 == hGframe_3_dot + mrotor_3 - cross(rGO_3, fframe_3), [M_Ox, M_Oy, M_Oz]); %Mframe_3 = [M_Ox; M_Oy; M_Oz];
-mframe_3 = [simplify(M_Ox), simplify(M_Oy), simplify(M_Oz)];
-    
-% RECALL - zero_reaction = [M_Gz; M_Ox; M_Oy; M_Oz] == 0 give our 4 eom's;
-eom1 = mrotor_4(3)==0;
-eom2 = mframe_3(1)==0;
-eom3 = mframe_3(2)==0;
-eom4 = mframe_3(3)==0;
-EOMS = [eom1; eom2; eom3; eom4];
-
-syms al be ga de 
-syms al_d be_d ga_d de_d
-syms al_dd be_dd ga_dd de_dd
-
-EOMS(1) = subs(EOMS(1), [alpha_, beta_, gamma_, delta_,  diff(alpha_,t), diff(beta_,t), diff(gamma_, t), diff(delta_,t), diff(alpha_,t, t), diff(beta_,t, t), diff(gamma_, t, t), diff(delta_,t, t) ], ...
-    [al, be, ga, de, al_d, be_d, ga_d, de_d, al_dd, be_dd, ga_dd, de_dd]);
-
-EOMS(2) = subs(EOMS(2), [alpha_, beta_, gamma_, delta_, diff(alpha_,t), diff(beta_,t), diff(gamma_, t), diff(delta_,t), diff(alpha_,t, t), diff(beta_,t, t), diff(gamma_, t, t), diff(delta_,t, t) ], ...
-    [al, be, ga, de, al_d, be_d, ga_d, de_d, al_dd, be_dd, ga_dd, de_dd]);
-
-EOMS(3) = subs(EOMS(3), [alpha_, beta_, gamma_, delta_, diff(alpha_,t), diff(beta_,t), diff(gamma_, t), diff(delta_,t), diff(alpha_,t, t), diff(beta_,t, t), diff(gamma_, t, t), diff(delta_,t, t) ], ...
-    [al, be, ga, de, al_d, be_d, ga_d, de_d, al_dd, be_dd, ga_dd, de_dd]);
-
-EOMS(4) = subs(EOMS(4), [alpha_, beta_, gamma_, delta_, diff(alpha_,t), diff(beta_,t), diff(gamma_, t), diff(delta_,t), diff(alpha_,t, t), diff(beta_,t, t), diff(gamma_, t, t), diff(delta_,t, t) ], ...
-    [al, be, ga, de, al_d, be_d, ga_d, de_d, al_dd, be_dd, ga_dd, de_dd]);
-
-vars = [al_dd, be_dd, ga_dd, de_dd];
-
-[A,b] = equationsToMatrix(EOMS, vars);
-X = A\b;
-
-al_dd = simplify(X(1));
-be_dd = simplify(X(2));
-ga_dd = simplify(X(3));
-de_dd = simplify(X(4));
-
-
-%% ---------------- solviong EOMs after filling in template. ----------------
-%forces on rotor
-% [F_Gx, F_Gy, F_Gz] = solve(m_rotor*rOG_4_dotdot == FGrotor_4 + Frotor_4,[F_Gx, F_Gy, F_Gz]) ;
-% frotor_4 = [expand(F_Gx); expand(F_Gy); expand(F_Gz)];
-%forces on frame
-% frotor_3 = R34*frotor_4;
-% 
-% [F_Ox, F_Oy, F_Oz] = solve(Fframe_3 + FGframe_3 - frotor_3 == m_frame*rOG_3_dotdot, [F_Ox, F_Oy, F_Oz]);
-% fframe_3 = [expand(F_Ox); expand(F_Oy); expand(F_Oz)];
-%moments on rotor % do in {3} - wait i dont think there are frame units if
-%applying cancels to both sides to put both in 3 cancels.
-% [M_Gx, M_Gy, M_Gz] = solve(hGrotor_4_dot == Mrotor_4,[M_Gx, M_Gy, M_Gz]);
-% mrotor_4 = [expand(M_Gx); expand(M_Gy); expand(M_Gz)];
-
-%moments on frame
-
-% mrotor_3 = R34*mrotor_4;
-% [M_Ox, M_Oy, M_Oz] = solve(hGframe_3_dot == ...
-%     Mframe_3 - mrotor_3 + cross(rGO_3, fframe_3), [M_Ox, M_Oy, M_Oz]);
-% mframe_3 = [expand(M_Ox); expand(M_Oy); expand(M_Oz)];
-% eom1 = mrotor_4(3)==0; 
-% eom2 = mframe_3(1)==0; 
-% eom3 = mframe_3(2)==0;
-% eom4 = mframe_3(3)==0;
-% equations = [eom1;eom2;eom3;eom4]; % 
-
-% equations(1) = subs(equations(1), [alpha_, beta_, gamma_, delta_,  diff(alpha_,t), diff(beta_,t), diff(gamma_, t), diff(delta_,t), diff(alpha_,t, t), diff(beta_,t, t), diff(gamma_, t, t), diff(delta_,t, t) ], ...
-%     [al, be, ga, de, al_d, be_d, ga_d, de_d, al_dd, be_dd, ga_dd, de_dd]);
-% 
-% equations(2) = subs(equations(2), [alpha_, beta_, gamma_, delta_, diff(alpha_,t), diff(beta_,t), diff(gamma_, t), diff(delta_,t), diff(alpha_,t, t), diff(beta_,t, t), diff(gamma_, t, t), diff(delta_,t, t) ], ...
-%     [al, be, ga, de, al_d, be_d, ga_d, de_d, al_dd, be_dd, ga_dd, de_dd]);
-% 
-% equations(3) = subs(equations(3), [alpha_, beta_, gamma_, delta_, diff(alpha_,t), diff(beta_,t), diff(gamma_, t), diff(delta_,t), diff(alpha_,t, t), diff(beta_,t, t), diff(gamma_, t, t), diff(delta_,t, t) ], ...
-%     [al, be, ga, de, al_d, be_d, ga_d, de_d, al_dd, be_dd, ga_dd, de_dd]);
-% 
-% equations(4) = subs(equations(4), [alpha_, beta_, gamma_, delta_, diff(alpha_,t), diff(beta_,t), diff(gamma_, t), diff(delta_,t), diff(alpha_,t, t), diff(beta_,t, t), diff(gamma_, t, t), diff(delta_,t, t) ], ...
-%     [al, be, ga, de, al_d, be_d, ga_d, de_d, al_dd, be_dd, ga_dd, de_dd]);
-% 
-% vars = [al_dd, be_dd, ga_dd, de_dd];
-% equations
-% [A,b] = equationsToMatrix(equations, vars);
-% X = A\b;
-% al_dd = simplify(X(1));
-% be_dd = simplify(X(2));
-% ga_dd = simplify(X(3));
-% de_dd = simplify(X(4));
 
 
 
 
 
 
-%% EOF
